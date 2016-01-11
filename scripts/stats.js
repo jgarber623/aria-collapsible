98de5a35b096b2a2228c62121700c8674882870d
Jason Garber <jason@sixtwothree.org>
2016-01-11 14:10:22 -0500

    Add comma-number to stats script.

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var cliTable = require('cli-table'),
+	commaNumber = require('comma-number'),
 	fs = require('fs'),
 	gzipSize = require('gzip-size'),
 	table = new cliTable({
@@ -10,9 +11,9 @@ var cliTable = require('cli-table'),
 	});
 
 table.push(
-	{ 'Uncompressed': fs.statSync('dist/aria-collapsible.js').size + ' bytes' },
-	{ 'Minified': fs.statSync('dist/aria-collapsible.min.js').size + ' bytes' },
-	{ 'Minified and gzipped': gzipSize.sync(fs.readFileSync('dist/aria-collapsible.min.js')) + ' bytes' }
+	{ 'Uncompressed': commaNumber(fs.statSync('dist/aria-collapsible.js').size) + ' bytes' },
+	{ 'Minified': commaNumber(fs.statSync('dist/aria-collapsible.min.js').size) + ' bytes' },
+	{ 'Minified and gzipped': commaNumber(gzipSize.sync(fs.readFileSync('dist/aria-collapsible.min.js'))) + ' bytes' }
 );
 
 console.log(table.toString());
